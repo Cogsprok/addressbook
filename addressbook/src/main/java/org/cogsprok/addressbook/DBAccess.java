74b98893ad47f008142ad622754f982a55deff46
John Wilson <jowilhnson@gmail.com>
2014-06-24 12:18:01 -0700

    basic DB operations written, ready for heavy refactoring before tying to existing application functions.

--- addressbook/src/main/java/org/cogsprok/addressbook/DBAccess.java
+++ addressbook/src/main/java/org/cogsprok/addressbook/DBAccess.java
@@ -5,19 +5,18 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 
 public class DBAccess {
 	
 	
 	
 	private Connection connect = null;
-	private Statement statement = null;
 	private PreparedStatement preparedStatement = null;
 	private ResultSet resultSet = null;
+	private TreeMap<String, String> conNames = new TreeMap<String, String>();
 	
 	
 
@@ -68,7 +67,9 @@ public class DBAccess {
 				.prepareStatement("SELECT FNAME, LNAME, ADDRESS, PHONE, EMAIL, TITLE, ORG FROM addressbook.BIZCONTACTS");
 		resultSet = preparedStatement.executeQuery();
 		HashMap<String, String> contact = writeResults(resultSet);
+		close(connect);
 		return contact;
+		
 	}
 	
 	private HashMap<String, String> writeResults (ResultSet results) throws SQLException {
@@ -100,6 +101,65 @@ public class DBAccess {
 			// don't throw now as it might leave following closables in undefined state
 		}
 		
+	}
+
+	public void addPersContact(String[] contact) throws Exception {
+		try {
+			connect = DriverManager
+					.getConnection("jdbc:mysql://localhost/addressbook?"
+							+ "user=adbook&password=adbook");
+		preparedStatement = connect
+				.prepareStatement("insert into addressbook.PERSCONTACTS values (default, ?, ?, ?, ?, ?, ?)");
+		preparedStatement.setString(1, contact[0]);
+		preparedStatement.setString(2, contact[1]);
+		preparedStatement.setString(3, contact[2]);
+		preparedStatement.setString(4, contact[3]);
+		preparedStatement.setString(5, contact[4]);
+		preparedStatement.setString(6, contact[5]);
+		preparedStatement.executeUpdate();
+		
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			close(connect);
+		}
+		
+	}
+
+	public HashMap<String, String> readPersContact() throws Exception {
+		connect = DriverManager
+				.getConnection("jdbc:mysql://localhost/addressbook?"
+						+ "user=adbook&password=adbook");
+		preparedStatement = connect
+				.prepareStatement("SELECT FNAME, LNAME, ADDRESS, PHONE, EMAIL, DOB FROM addressbook.PERSCONTACTS");
+		resultSet = preparedStatement.executeQuery();
+		HashMap<String, String> contact = writeResults(resultSet);
+		close(connect);
+		return contact;
+	}
+
+	public TreeMap<String, String> listContacts() throws Exception {
+		conNames.clear();
+		connect = DriverManager
+				.getConnection("jdbc:mysql://localhost/addressbook?"
+						+ "user=adbook&password=adbook");
+		preparedStatement = connect
+				.prepareStatement("SELECT LNAME, FNAME FROM addressbook.BIZCONTACTS"); 
+		resultSet = preparedStatement.executeQuery();
+		mapNames(resultSet);
+		preparedStatement = connect
+				.prepareStatement("SELECT LNAME, FNAME FROM addressbook.PERSCONTACTS;");
+		resultSet = preparedStatement.executeQuery();
+		mapNames(resultSet);
+	
+		close(connect);
+		return conNames;
+	}
+	
+	private void mapNames(ResultSet results) throws SQLException {
+		while (results.next()) {
+			conNames.put(results.getString("LNAME"), results.getString("FNAME"));
+			}
 	}
 	
 	
--- addressbook/src/test/java/org/cogsprok/addressbook/TestDBAccess.java
+++ addressbook/src/test/java/org/cogsprok/addressbook/TestDBAccess.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.util.HashMap;
 import java.util.Map.Entry;
+import java.util.TreeMap;
 
 import org.junit.Test;
 
@@ -58,6 +59,59 @@ public class TestDBAccess {
 			fail("Exception was thrown");
 			
 		}
+		
+	}
+	
+	@Test
+	public void testWritePersContact() {
+		boolean noException = false;
+		String[] contact = {"pTestFN", "pTestLN", "pTestAdd", "pTestPhone", "pTestEm", "1976-06-03" };
+		try {
+			dba.addPersContact(contact);
+			noException = true;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			noException = false;
+		}
+		assertTrue(noException);
+		
+	}
+	
+	@Test
+	public void testReadPersContact() {
+		try {
+		HashMap<String, String> pers = dba.readPersContact();
+		assertEquals("pTestFN", pers.get("fname"));
+		assertEquals("pTestLN", pers.get("lname"));
+		assertEquals("pTestAdd", pers.get("address"));
+		for(Entry<String, String> entry : pers.entrySet()) {
+			System.out.println(entry.getKey() + " " + entry.getValue());
+		}
+		
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			fail("Exception was thrown");
+			
+		}
+		
+	}
+	
+	@Test
+	public void testDisplayContacts() {
+		try {
+			TreeMap<String, String> cons = dba.listContacts();
+			for(Entry<String, String> entry : cons.entrySet()) {
+				System.out.println(entry.getKey() + ", " + entry.getValue());
+			} 
+			assertEquals("jTestFN", cons.get("jTestLN"));
+			assertEquals("pTestFN", cons.get("pTestLN"));
+		
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+			fail("Exception was thrown in DisplayContacts");
+		}
+		
+	
 		
 	}
 
